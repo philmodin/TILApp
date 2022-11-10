1f7d283d096592b2f985e040ff7526bfc6323ef3
Philip Modin <18331871+philmodin@users.noreply.github.com>
2022-11-09 19:22:39 -0600

    Chapter 18 Complete

---
+++ Sources/App/Migrations/CreateAdminUser.swift
@@ -0,0 +1,28 @@
+//
+//  CreateAdminUser.swift
+//  
+//
+//  Created by Philip Modin on 11/9/22.
+//
+
+import Fluent
+import Vapor
+
+struct CreateAdminUser: Migration {
+	func prepare(on database: Database) -> EventLoopFuture<Void> {
+		let passwordHash: String
+		do {
+			passwordHash = try Bcrypt.hash("password")
+		} catch {
+			return database.eventLoop.future(error: error)
+		}
+		let user = User(name: "Admin", username: "admin", password: passwordHash)
+		return user.save(on: database)
+	}
+	
+	func revert(on database: Database) -> EventLoopFuture<Void> {
+		User.query(on: database)
+			.filter(\.$username == "admin")
+			.delete()
+	}
+}
